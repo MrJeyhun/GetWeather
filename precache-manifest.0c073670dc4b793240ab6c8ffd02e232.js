6903d3c49e02c3baba4facceebc970b094528c22
Jeyhun Jeyhunzade <ceyhun.zade1998@gmail.com>
2019-12-13 05:53:33 +0400

    Updates

---
+++ precache-manifest.0c073670dc4b793240ab6c8ffd02e232.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119118c6b075095f247313e7da7bad6c",
+    "url": "/GetWeather/index.html"
+  },
+  {
+    "revision": "c185e767b25e5e3dac8f",
+    "url": "/GetWeather/static/css/2.21c3bd60.chunk.css"
+  },
+  {
+    "revision": "04d30caa303261cce4b1",
+    "url": "/GetWeather/static/css/main.21e342f8.chunk.css"
+  },
+  {
+    "revision": "c185e767b25e5e3dac8f",
+    "url": "/GetWeather/static/js/2.0eae9fbe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/GetWeather/static/js/2.0eae9fbe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04d30caa303261cce4b1",
+    "url": "/GetWeather/static/js/main.21c462d3.chunk.js"
+  },
+  {
+    "revision": "1775752bf6aab5832701",
+    "url": "/GetWeather/static/js/runtime-main.f537728c.js"
+  },
+  {
+    "revision": "fa8b05e9a54cbf89b664d56a5efe04d3",
+    "url": "/GetWeather/static/media/weather-wallpaper-4.fa8b05e9.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/GetWeather/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/GetWeather/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/GetWeather/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/GetWeather/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/GetWeather/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
